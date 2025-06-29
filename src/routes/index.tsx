fe00906a6ece996456ee17f7839ada823b7d7277
dumisa-sakhile <contact.dumisa@gmail.com>
2025-06-29 03:49:30 +0200

    adjust mobile font size

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -86,7 +86,7 @@ function App() {
             * POWERED BY AI *
           </motion.div>
           <motion.h1
-            className="text-5xl sm:text-6xl font-bold text-white mb-4"
+            className=" text-2xl md:text-5xl font-bold text-white mb-4"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.4, duration: 0.8 }}>
