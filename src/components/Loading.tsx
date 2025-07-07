8bdbad61d091b8132dc9d53d7478ae7879b31194
dumisa-sakhile <contact.dumisa@gmail.com>
2025-07-07 23:59:28 +0200

    remove loading bg

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center py-6 min-h-[10rem] bg-black transition-colors">
+    <div className="w-full h-full flex items-center justify-center py-6 min-h-[10rem]  transition-colors">
       {/* Custom Tailwind CSS Spinner */}
       <div className="w-12 h-12 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin" />
     </div>
