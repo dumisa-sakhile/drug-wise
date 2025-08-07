ac373ede80773c4e77b39a9b92046a914a4ac32d
dumisa-sakhile <contact.dumisa@gmail.com>
2025-08-07 22:30:35 +0200

    change button bg to lime

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -138,7 +138,7 @@ function Login() {
       {/* Background Gradients/Effects added here */}
       <div className="absolute top-0 left-0 w-full h-full pointer-events-none opacity-20 z-0 overflow-hidden">
         <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob" />
-        <div className="absolute top-1/2 right-1/4 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000" />
+        <div className="absolute top-1/2 right-1/4 w-96 h-96 bg-lime-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000" />
         <div className="absolute bottom-1/4 left-1/3 w-96 h-96 bg-teal-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-4000" />
       </div>
 
@@ -190,7 +190,7 @@ function Login() {
                   window.localStorage.getItem("emailForSignIn") || ""
                 )}
                 rel="noopener noreferrer"
-                className={`${buttonBaseStyles} bg-blue-600 hover:bg-blue-700 text-white inline-block`}
+                className={`${buttonBaseStyles} bg-lime-600 hover:bg-lime-700 text-white inline-block`}
                 initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ delay: 0.6, duration: 0.5 }}>
@@ -261,10 +261,10 @@ function Login() {
                 <motion.button
                   type="submit"
                   disabled={isLoading || !isValidEmail || !email}
-                  className={`${buttonBaseStyles} bg-blue-600 text-white mb-4 ${
+                  className={`${buttonBaseStyles} bg-lime-600 hover:bg-lime-800 text-white mb-4 ${
                     isLoading || !isValidEmail || !email
                       ? disabledStyles
-                      : "hover:bg-blue-700"
+                      : "hover:bg-lime-900 cursor-not-allowed"
                   }`}
                   initial={{ opacity: 0, scale: 0.95 }}
                   animate={{ opacity: 1, scale: 1 }}
